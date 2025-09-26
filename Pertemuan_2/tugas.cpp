66757ac0a671edb24c90ecebe7f5b880c742be06
Rathal0s <145148352+Rathal0s@users.noreply.github.com>
2025-09-26 16:41:27 +0700

    Add files via upload

---
+++ Pertemuan_2/tugas.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double A, B;
+
+    cout << "Jumlah A: ";
+    cin >> A;
+    cout << "Jumlah B: ";
+    cin >> B;
+
+
+    cout<< "No 1. Pertambahan"<<endl;
+    cout << "A = " << A <<endl;
+    cout << "B = " << B <<endl;
+
+    cout << "A + B = " << A + B <<endl;
+    cout << "Hasil A + B = " << A + B <<endl<<endl<<endl<<endl;
+
+    cout<< "No 2. Pengurangan"<<endl;
+     cout << "A = " << A <<endl;
+    cout << "B = " << B <<endl;
+
+    
+    cout << "A - B = " << A - B <<endl;
+    cout << "Hasil A - B = " << A - B <<endl<<endl<<endl<<endl;
+
+    cout<< "No 3. Perkalian"<<endl;
+     cout << "A = " << A <<endl;
+    cout << "B = " << B <<endl;
+
+    cout << "A * B = " << A * B <<endl;
+    cout << "Hasil A * B = " << A * B <<endl<<endl<<endl<<endl;
+
+    cout<< "No 4. Pembagian"<<endl;
+    cout << "A = " << A <<endl;
+    cout << "B = " << B <<endl;
+
+    cout << "A / B = " << A / B <<endl;
+    cout << "Hasil A / B = " << A / B <<endl<<endl<<endl<<endl;
+
+}
