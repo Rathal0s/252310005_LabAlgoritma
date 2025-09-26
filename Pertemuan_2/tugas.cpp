e2d407ceeda7f0ec340c67120d41450cfdc7410f
Rathal0s <145148352+Rathal0s@users.noreply.github.com>
2025-09-26 17:11:15 +0700

    Update tugas.cpp
    Added cin

--- Pertemuan_2/tugas.cpp
+++ Pertemuan_2/tugas.cpp
@@ -4,39 +4,50 @@ using namespace std;
 int main() {
     double A, B;
 
+
+    
+    cout << "No 1. Pertambahan" << endl;
     cout << "Jumlah A: ";
     cin >> A;
     cout << "Jumlah B: ";
     cin >> B;
+    cout << "A = " << A << endl;
+    cout << "B = " << B << endl;
+    cout << "A + B = " << A + B << endl;
+    cout << "Hasil A + B = " << A + B << endl << endl << endl << endl;
 
 
-    cout<< "No 1. Pertambahan"<<endl;
-    cout << "A = " << A <<endl;
-    cout << "B = " << B <<endl;
 
-    cout << "A + B = " << A + B <<endl;
-    cout << "Hasil A + B = " << A + B <<endl<<endl<<endl<<endl;
+    cout << "No 2. Pengurangan" << endl;
+    cout << "Jumlah A: ";
+    cin >> A;
+    cout << "Jumlah B: ";
+    cin >> B;
+    cout << "A = " << A << endl;
+    cout << "B = " << B << endl;
+    cout << "A - B = " << A - B << endl;
+    cout << "Hasil A - B = " << A - B << endl << endl << endl << endl;
 
-    cout<< "No 2. Pengurangan"<<endl;
-     cout << "A = " << A <<endl;
-    cout << "B = " << B <<endl;
 
-    
-    cout << "A - B = " << A - B <<endl;
-    cout << "Hasil A - B = " << A - B <<endl<<endl<<endl<<endl;
+    cout << "No 3. Perkalian" << endl;
+    cout << "Jumlah A: ";
+    cin >> A;
+    cout << "Jumlah B: ";
+    cin >> B;
+    cout << "A = " << A << endl;
+    cout << "B = " << B << endl;
+    cout << "A * B = " << A * B << endl;
+    cout << "Hasil A * B = " << A * B << endl << endl << endl << endl;
 
-    cout<< "No 3. Perkalian"<<endl;
-     cout << "A = " << A <<endl;
-    cout << "B = " << B <<endl;
 
-    cout << "A * B = " << A * B <<endl;
-    cout << "Hasil A * B = " << A * B <<endl<<endl<<endl<<endl;
-
-    cout<< "No 4. Pembagian"<<endl;
-    cout << "A = " << A <<endl;
-    cout << "B = " << B <<endl;
-
-    cout << "A / B = " << A / B <<endl;
-    cout << "Hasil A / B = " << A / B <<endl<<endl<<endl<<endl;
+    cout << "No 4. Pembagian" << endl;
+    cout << "Jumlah A: ";
+    cin >> A;
+    cout << "Jumlah B: ";
+    cin >> B;
+    cout << "A = " << A << endl;
+    cout << "B = " << B << endl;
+    cout << "A / B = " << A / B << endl;
+    cout << "Hasil A / B = " << A / B << endl << endl << endl << endl;
 
 }
